0d406b6273ec46221382f9a91c24fdcebdece9e3
Ben <benaclejames@gmail.com>
2022-04-16 12:56:08 -0700

    Removed unused imports. Added zero deposit check to pay_money to avoid unnecessary storage and processing. Modified money accrued check from accrued > goal to accrued >= goal. Replaced get_records code with a more simple return of the to_vec func.

--- party-bid/contract/src/lib.rs
+++ party-bid/contract/src/lib.rs
@@ -1,55 +1,41 @@
 use near_sdk::borsh::{self, BorshDeserialize, BorshSerialize};
-use near_sdk::{env, near_bindgen, setup_alloc, AccountId, Promise};
-use near_sdk::{ext_contract};
 use near_sdk::serde::{Deserialize, Serialize};
 use near_sdk::serde_json;
-use near_sdk::collections::UnorderedMap;
+use near_sdk::{env, near_bindgen, setup_alloc, Promise};
 
 setup_alloc!();
 
 #[derive(Serialize, Deserialize, BorshDeserialize, BorshSerialize)]
 #[serde(crate = "near_sdk::serde")]
 pub struct DeployArgs {
-  money_goal: u128,
+    money_goal: u128,
 }
-
 
 #[near_bindgen]
 #[derive(BorshDeserialize, BorshSerialize)]
-pub struct Factory {
-    
-}
-
-impl Default for Factory {
-    fn default() -> Self {
-      Self {
-        
-      }
-    }
-  }
+pub struct Factory;
 
 
-unsafe fn any_as_u8_slice<T: Sized>(p: &T) -> &[u8] {
-    ::std::slice::from_raw_parts(
-        (p as *const T) as *const u8,
-        ::std::mem::size_of::<T>(),
-    )
-}
 
 const CODE: &[u8] = include_bytes!("./greeter.wasm");
 
+
+
 #[near_bindgen]
 impl Factory {
+    pub fn deploy(self, money_goal: u128) {
+        let init_args = &DeployArgs { money_goal };
 
-    pub fn deploy(self, money_goal: u128){
-      let init_args = &DeployArgs {money_goal: money_goal};
         Promise::new("dev-1649971938317-53315658254381".parse().unwrap())
-        .create_account()
-        .add_full_access_key(env::signer_account_pk())
-        .transfer(1_000_000_000_000_000_000_000_0000)
-        .deploy_contract(CODE.to_vec())
-        .function_call(b"new".to_vec(), serde_json::to_vec(init_args).unwrap(), 0, 1_000_000_000_0000);
-
-
+            .create_account()
+            .add_full_access_key(env::signer_account_pk())
+            .transfer(1_000_000_000_000_000_000_000_0000)
+            .deploy_contract(CODE.to_vec())
+            .function_call(
+                b"new".to_vec(),
+                serde_json::to_vec(init_args).unwrap(),
+                0,
+                1_000_000_000_0000,
+            );
     }
 }
