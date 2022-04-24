e67a8a9e5bc2234bcf35ab6685bb3faa91cc87fb
Jerry Ye <jerry1ye10@gmail.com>
2022-04-24 13:43:00 -0700

    update factory contract

--- party-bid/contracts/factory/src/lib.rs
+++ party-bid/contracts/factory/src/lib.rs
@@ -1,15 +1,18 @@
 use near_sdk::borsh::{self, BorshDeserialize, BorshSerialize};
 use near_sdk::serde::{Deserialize, Serialize};
 use near_sdk::serde_json;
-use near_sdk::{env, near_bindgen, setup_alloc, Promise};
+use near_sdk::{env, near_bindgen, setup_alloc, Promise, Gas};
 
 setup_alloc!();
+
 
 #[derive(Serialize, Deserialize, BorshDeserialize, BorshSerialize)]
 #[serde(crate = "near_sdk::serde")]
 pub struct DeployArgs {
-    money_goal: u128,
+    money_goal: String,
     nft_id: String,
+    name: String, 
+    symbol: String
 }
 
 #[near_bindgen]
@@ -17,23 +20,26 @@ pub struct DeployArgs {
 pub struct Factory;
 
 impl Default for Factory {
-    fn default() -> Self {
-        Self {
+  fn default() -> Self {
+    Self {
 
-        }
     }
+  }
 }
 
-const CODE: &[u8] = include_bytes!("../../party/target/wasm32-unknown-unknown/debug/party.wasm");
+const CODE: &[u8] = include_bytes!("./greeter.wasm");
 
 
 
 #[near_bindgen]
 impl Factory {
-    pub fn deploy(self, money_goal: u128, nft_id: String ) {
-        let mut account_id: String = "dev-1650291992640-60218357254222".to_string();
+
+    pub fn deploy(self, money_goal: String, nft_id: String ) -> String {
+        let mut account_id: String = "dev-1650291992643-60218357254228".to_string();
         account_id.push_str(&nft_id);
-        let init_args = &DeployArgs { money_goal: money_goal, nft_id: nft_id };
+        let init_args = &DeployArgs { money_goal: money_goal, nft_id: nft_id, name: "jerry".to_string(), symbol: "jerry".to_string()};
+
+        let gas: Gas = 250_000_000_000_000.into();
 
         Promise::new(account_id.parse().unwrap())
             .create_account()
@@ -41,10 +47,12 @@ impl Factory {
             .transfer(1_000_000_000_000_000_000_000_0000)
             .deploy_contract(CODE.to_vec())
             .function_call(
-                b"new".to_vec(),
+                "new".to_string(),
                 serde_json::to_vec(init_args).unwrap(),
                 0,
-                1_000_000_000_0000,
+                gas,
+            ).then(
+              return "Success".to_string()
             );
     }
 }
